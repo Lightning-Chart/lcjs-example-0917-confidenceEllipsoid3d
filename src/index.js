8204d8f037cf9d82d4d04a31987fb2d762bc8f35
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:11:51 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
 const lcjs = require('@arction/lcjs')
 const { lightningChart, PointStyle3D, ColorShadingStyles, Themes } = lcjs
 
-const chart3D = lightningChart()
+const chart3D = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .Chart3D({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
